3d7aa9fafff941b713203ea456897ad5e3bacf1f
Adrián Martínez <59429351+AdrianMartinezT@users.noreply.github.com>
2023-11-29 18:05:18 -0800

    Add files via upload

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -14,6 +14,7 @@ import {
   GridItem,
   Center,
   Grid,
+  
 } from "@chakra-ui/react";
 import { useApi, useAlert, useAccount } from "@gear-js/react-hooks";
 import { useState,useEffect } from "react";
